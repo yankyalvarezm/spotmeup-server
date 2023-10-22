aa5b7a487563fa62e10dac3474fd323c7f5ba607
yankyalvarezm <alvarezyanky7#gmail.com>
2023-10-22 18:41:41 -0400

    added new features

--- models/Venue.js
+++ models/Venue.js
@@ -4,8 +4,12 @@ const venueSchema = new Schema(
   {
     name: String,
     address: String,
+    state: String,
+    city: String,
+    zipcode: Number,
     owner: String,
     capacity: Number,
+    description: String,
     layout: [{ type: Schema.Types.ObjectId, ref: "Layout" }],
   },
   {
--- routes/venue.routes.js
+++ routes/venue.routes.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const Venue = require("../models/Venue");
 const Layout = require("../models/Layout");
 
+// --------------- Create Venue -------------
+
 router.post("/create", async (req, res, next) => {
   try {
     const { name, address, owner, capacity, layout } = req.body;
@@ -28,6 +30,27 @@ router.post("/create", async (req, res, next) => {
   }
 });
 
+// --------------- Delete Venue -------------
+
+router.delete("/delete/:venueId", async (req, res, next) => {
+  try {
+    const { venueId } = req.params;
+
+    await Venue.findByIdAndDelete(venueId);
+
+    const updatedVenues = await Venue.find({});
+
+    res.json(updatedVenues);
+
+    console.log("Line 44 - Deleted Venue:", venueId);
+  } catch (err) {
+    console.log("Line 45 - Error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// --------------- Add Layout -------------
+
 router.post("/addlayout/:venueId", async (req, res) => {
   const { venueId } = req.params;
   const { layout } = req.body;
@@ -49,6 +72,59 @@ router.post("/addlayout/:venueId", async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Error adding layout" + error });
+  }
+});
+
+// --------------- Find All Venues -------------
+
+router.get("/allvenues", async (req, res, next) => {
+  try {
+    const existVenue = await Venue.find();
+    if (existVenue && existVenue.length > 0) {
+      res.json({ success: true, venue: existVenue });
+    } else {
+      res.json({ success: false, msg: "No venues found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Error loading venues" + error });
+  }
+});
+
+// --------------- Find Venue by Id -------------
+
+router.get("/allvenues/:venueId", async (req, res, next) => {
+  const { venueId } = req.params;
+
+  try {
+    const existVenue = await Venue.findById(venueId);
+    if (existVenue) {
+      res.json({ success: true, venue: existVenue });
+    } else {
+      res.json({ success: false, msg: "Venue not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Error loading venues" + error });
+  }
+});
+
+// --------------- Edit Venue -------------
+
+router.put("/edit/:venueId", async (req, res, next) => {
+  try {
+    const { venueId } = req.params;
+    const updatedData = req.body;
+
+    await Venue.findByIdAndUpdate(venueId, updatedData, { new: true });
+
+    const updatedVenues = await Venue.findById(venueId);
+    res.json(updatedVenues);
+
+    console.log("Line 124 - Edited Venue:", venueId);
+  } catch (err) {
+    console.log("Line 126 - Error:", err);
+    res.status(500).send("Internal Server Error");
   }
 });
 
